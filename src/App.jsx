c3a1aa2afcd259cb987d6d68f0cc11c852b40fc3
sawyerno <sawyernoland2005@gmail.com>
2023-09-03 18:53:02 -0400

    final touches- finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-
+import {puppyList} from './data.js'
+import { useState } from 'react'
 import './App.css'
 
+
+
 function App() {
-  
+  console.log(puppyList)
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+console.log(featuredPup);
   return (
     <>
-      <div>
-        
+      <div className='myApp'>
+        {puppies.map((puppy) => {
+          return <p onClick= {()=>{ setFeatPupId(puppy.id)}}
+          key={puppy.id}>{puppy.name}</p>
+        })
+        }
+         {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       </div>
     </>
   )
